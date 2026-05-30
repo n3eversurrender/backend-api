8c71d5cfbcda4ff87fe8b70101b21eb119194510
Zaini <zainiridha8@Gmail.com>
2026-05-30 17:29:55 +0700

    feat: initialize Sequelize database configuration with environment variable support

--- src/database/config.js
+++ src/database/config.js
@@ -1,10 +1,22 @@
-require('dotenv');
+require('dotenv').config();
+
 module.exports = {
-  dialect: process.env.DB_DRIVER,
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  logging: false,
+  development: {
+    dialect: process.env.DB_DRIVER || 'mysql',
+    host: process.env.DB_HOST || 'localhost',
+    port: +process.env.DB_PORT || 3306,
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || '',
+    logging: false,
+  },
+  production: {
+    dialect: process.env.DB_DRIVER || 'mysql',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT || 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    logging: false,
+  },
 };
